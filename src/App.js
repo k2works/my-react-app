087160e622a9360acdc19b8ca607e8fc5252d4c4
k2works <kakimomokuri@gmail.com>
2016-10-29 16:04:44 +0900

    State and Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
 
 class App extends Component {
-  render() {
+    render() {
     return (
       <div className="App">
         <div className="App-header">
@@ -11,16 +12,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-            <h1>Hello, world!</h1>
-            {element2_1}
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
-            <Comment
-                date={comment.date}
-                text={comment.text}
-                author={comment.author}
-            />
+            <Clock />
+            <Clock />
+            <Clock />
         </p>
       </div>
     );
@@ -28,44 +22,3 @@ class App extends Component {
 }
 
 export default App;
-
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
-
-const element2_1 = <Welcome name="Sara" />;
-
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <div className="UserInfo">
-                <img className="Avatar"
-                     src={props.author.avatarUrl}
-                     alt={props.author.name}
-                />
-                <div className="UserInfo-name">
-                    {props.author.name}
-                </div>
-            </div>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
-
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-    }
-};
