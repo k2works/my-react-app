56f6d73f3b9c89f258b8e53b4d6eb4feea649988
k2works <kakimomokuri@gmail.com>
2016-10-29 16:58:19 +0900

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './Toggle'
-import LoggingButton from './LoggingButton'
 
 class App extends Component {
     render() {
@@ -14,9 +12,10 @@ class App extends Component {
         </div>
         <p className="App-intro">
             <h1>Hello, world!</h1>
-            <ActionLink /><br/>
-            <Toggle /><br/>
-            <LoggingButton />
+            <Greeting isLoggedIn={false} />
+            <LoginControl />
+            <Mailbox unreadMessages={messages} />
+            <Page />
         </p>
       </div>
     );
@@ -25,15 +24,117 @@ class App extends Component {
 
 export default App;
 
-function ActionLink() {
-    function handleClick(e) {
-        e.preventDefault();
-        console.log('The link was clicked.');
+function WarningBanner(props) {
+    if (!props.warn) {
+        return null;
     }
 
     return (
-        <a href="#" onClick={handleClick}>
-            Click me
-        </a>
+        <div className="warning">
+            Warning!
+        </div>
+    );
+}
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showWarning: true}
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+
+    handleToggleClick() {
+        this.setState(prevState => ({
+            showWarning: !prevState.showWarning
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <WarningBanner warn={this.state.showWarning} />
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
+            </div>
+        );
+    }
+}
+
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Hello!</h1>
+            {unreadMessages.length > 0 &&
+            <h2>
+                You have {unreadMessages.length} unread messages.
+            </h2>
+            }
+        </div>
+    );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        return (
+            <div>
+                {isLoggedIn ? (
+                    <LogoutButton onClick={this.handleLogoutClick}/>
+                ) : (
+                    <LoginButton onClick={this.handleLoginClick}/>
+                )}
+            </div>
+        );
+    }
+}
+
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
     );
 }
