532fe9c5f4174f3ebd8a786affd90c76d7a8158b
k2works <kakimomokuri@gmail.com>
2016-10-29 16:34:47 +0900

    Handling Events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './Toggle'
+import LoggingButton from './LoggingButton'
 
 class App extends Component {
     render() {
@@ -34,43 +36,4 @@ function ActionLink() {
             Click me
         </a>
     );
-}
-
-class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isToggleOn: true};
-
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
-    }
-
-    render() {
-        return (
-            <button onClick={this.handleClick}>
-                {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </button>
-        );
-    }
-}
-
-class LoggingButton extends React.Component {
-    handleClick() {
-        console.log('this is:', this);
-    }
-
-    render() {
-        // This syntax ensures `this` is bound within handleClick
-        return (
-            <button onClick={(e) => this.handleClick(e)}>
-                Click me
-            </button>
-        );
-    }
 }
