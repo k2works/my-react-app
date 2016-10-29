fed3a6797013522494a80e973c0ce6c278c5db6f
k2works <kakimomokuri@gmail.com>
2016-10-29 15:20:15 +0900

    Components and Props

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ class App extends Component {
         </div>
         <p className="App-intro">
             <h1>Hello, world!</h1>
-            {element}
-            {element1_2}
-            {element1_3}
-            {element1_4}
-            {element1_5}
-            {element1_6}
-            {element1_7}
             {element2_1}
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+            <Comment
+                date={comment.date}
+                text={comment.text}
+                author={comment.author}
+            />
         </p>
       </div>
     );
@@ -28,66 +29,43 @@ class App extends Component {
 
 export default App;
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
 
-var user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-};
+const element2_1 = <Welcome name="Sara" />;
 
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
-);
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
 
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>;
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar"
+                     src={props.author.avatarUrl}
+                     alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
     }
-    return <h1>Hello, Stranger.</h1>;
-}
-
-user = null;
-
-const element1_2 = (
-    <h1>
-        {getGreeting(user)}
-    </h1>
-);
-
-var user = {
-    firstName: 'k2works',
-    lastName: '',
-    avatarUrl: 'https://avatars0.githubusercontent.com/u/3236312?v=3&s=466'
 };
-
-const element1_3 = <div tabIndex="0"></div>;
-const element1_4 = <img src={user.avatarUrl}></img>;
-
-const element1_5 = (
-    <div>
-        <h1>Hello!</h1>
-        <h2>Good to see you here.</h2>
-    </div>
-);
-
-const element1_6 = (
-    <h1 className="greeting">
-        Hello, world!
-    </h1>
-);
-const element1_7 = React.createElement(
-    'h1',
-    {className: 'greeting'},
-    'Hello, world!'
-);
-
-const element2_1 = (
-    <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-);
--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function tick() {
-    const element2_1 = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(
-        element2_1,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    < App />,
+    document.getElementById('root')
+);
