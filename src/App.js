b4b378d715095ac15afc1d40b3e5fb696fcf3bd6
k2works <kakimomokuri@gmail.com>
2016-10-29 17:07:25 +0900

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page from './Page'
+import Mailbox from './Mailbox'
+import LoginControl from './LoginControl'
 
 class App extends Component {
     render() {
@@ -24,88 +27,7 @@ class App extends Component {
 
 export default App;
 
-function WarningBanner(props) {
-    if (!props.warn) {
-        return null;
-    }
-
-    return (
-        <div className="warning">
-            Warning!
-        </div>
-    );
-}
-
-class Page extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {showWarning: true}
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick() {
-        this.setState(prevState => ({
-            showWarning: !prevState.showWarning
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <WarningBanner warn={this.state.showWarning} />
-                <button onClick={this.handleToggleClick}>
-                    {this.state.showWarning ? 'Hide' : 'Show'}
-                </button>
-            </div>
-        );
-    }
-}
-
-function Mailbox(props) {
-    const unreadMessages = props.unreadMessages;
-    return (
-        <div>
-            <h1>Hello!</h1>
-            {unreadMessages.length > 0 &&
-            <h2>
-                You have {unreadMessages.length} unread messages.
-            </h2>
-            }
-        </div>
-    );
-}
-
 const messages = ['React', 'Re: React', 'Re:Re: React'];
-
-class LoginControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
-    }
-
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
-    }
-
-    render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        return (
-            <div>
-                {isLoggedIn ? (
-                    <LogoutButton onClick={this.handleLogoutClick}/>
-                ) : (
-                    <LoginButton onClick={this.handleLoginClick}/>
-                )}
-            </div>
-        );
-    }
-}
 
 function UserGreeting(props) {
     return <h1>Welcome back!</h1>;
@@ -121,20 +43,4 @@ function Greeting(props) {
         return <UserGreeting />;
     }
     return <GuestGreeting />;
-}
-
-function LoginButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Login
-        </button>
-    );
-}
-
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Logout
-        </button>
-    );
 }
---
+++ src/LoginControl.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        return (
+            <div>
+                {isLoggedIn ? (
+                    <LogoutButton onClick={this.handleLogoutClick}/>
+                ) : (
+                    <LoginButton onClick={this.handleLoginClick}/>
+                )}
+            </div>
+        );
+    }
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+export default LoginControl;
+
