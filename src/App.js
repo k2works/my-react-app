567aac5115c84f5126ed3ac393e4bae81cd1367b
k2works <kakimomokuri@gmail.com>
2016-10-29 17:32:07 +0900

    Lists and Keys

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Page from './Page'
-import Mailbox from './Mailbox'
-import LoginControl from './LoginControl'
 
 class App extends Component {
     render() {
@@ -15,10 +12,11 @@ class App extends Component {
         </div>
         <p className="App-intro">
             <h1>Hello, world!</h1>
-            <Greeting isLoggedIn={false} />
-            <LoginControl />
-            <Mailbox unreadMessages={messages} />
-            <Page />
+            <ListItem value={numbers} />
+            <NumberList numbers={numbers} />
+            <Blog posts={posts} />
+            <NumberList2 numbers={numbers} />
+            <NumberList3 numbers={numbers} />
         </p>
       </div>
     );
@@ -27,20 +25,78 @@ class App extends Component {
 
 export default App;
 
-const messages = ['React', 'Re: React', 'Re:Re: React'];
-
-function UserGreeting(props) {
-    return <h1>Welcome back!</h1>;
+function ListItem(props) {
+    // Correct! There is no need to specify the key here:
+    return <li>{props.value}</li>;
 }
 
-function GuestGreeting(props) {
-    return <h1>Please sign up.</h1>;
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        // Correct! Key should be specified inside the array.
+        <ListItem key={number.toString()}
+                  value={number} />
+    );
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    );
 }
 
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting />;
-    }
-    return <GuestGreeting />;
+const numbers = [1, 2, 3, 4, 5];
+
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
+    const content = props.posts.map((post) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+
+function NumberList2(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <ListItem key={number.toString()}
+                  value={number} />
+    );
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    );
+}
+
+function NumberList3(props) {
+    const numbers = props.numbers;
+    return (
+        <ul>
+            {numbers.map((number) =>
+                <ListItem key={number.toString()}
+                          value={number} />
+            )}
+        </ul>
+    );
 }
